4758bf9503c2e4fc0f19829ab6c33e6709dfd4dd
mananx22 <motorollaman27@gmail.com>
2021-06-22 01:06:32 +0000

    error correctios

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,5 +1,5 @@
-// use this to let your editor know you're using a groovy file
-#!/usr/bin/env groovy
+// use this to let your editor know you're using a groovy file, but it fails the multibranch pipeline so i commented
+// #!/usr/bin/env groovy
 
 def call() {
     echo "building our java application"
